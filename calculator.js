3d5cfce1f5a59752adc828da03090c58317daa06
Tobiaha <122260640+Tobiaha@users.noreply.github.com>
2025-12-09 18:10:57 +0200

    operator function

--- calculator.js
+++ calculator.js
@@ -1,18 +1,19 @@
 "use strict";
 const display = document.getElementById("display");
-const numButtons = document.querySelectorAll(".button.number");
-const operatorButtons = document.querySelectorAll(
-  ".button.operator, .button.decimal"
-);
+const numButtons = document.querySelectorAll("button.number");
+const operatorButtons = document.querySelectorAll("button.operator");
 
 const decimalButton = document.getElementById("decimalBtn");
 const clearButton = document.getElementById("clearBtn");
 const deleteButton = document.getElementById("deleteBtn");
 const equalButton = document.getElementById("equalBtn");
 
+let opArray = [];
+let firstNumber = "";
 let lastNumber = "";
+let lastNumTurn = false;
+
 let operator = "";
-let total = 0;
 
 const add = (a, b) => a + b;
 
@@ -22,86 +23,71 @@ const divide = (a, b) => a / b;
 
 const multiply = (a, b) => a * b;
 
-function calculate() {
-  const num = parseFloat(lastNumber);
-  if (!isNaN(num)) {
-    if (operator) {
-      switch (operator) {
-        case "+":
-          total += num;
-          break;
-        case "-":
-          total -= num;
-          break;
-        case "*":
-          total *= num;
-          break;
-        case "/":
-          total /= num;
-          break;
-      }
-    } else {
-      total = num;
-    }
+function calculate(firstNumber, lastNumber) {
+  let total = 0;
+  const firstNum = parseFloat(firstNumber);
+  const lastNum = parseFloat(lastNumber);
+
+  switch (operator) {
+    case "+":
+      total = firstNum + lastNum;
+      break;
+    case "-":
+      total = firstNum - lastNum;
+      break;
+    case "*":
+      total = firstNum * lastNum;
+      break;
+    case "/":
+      total = firstNum / lastNum;
+      break;
   }
+  console.log(total);
+  firstNum = total.toString();
+  lastNum = "";
+  operator = "";
+  lastNumTurn = false; // ready to type new number
 }
 
-//numbers
 numButtons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const storedNum = btn.textContent.trim();
+  btn.addEventListener("click", (e) => {
+    const numText = e.target.textContent;
+    if (!lastNumTurn && !operator) {
+      firstNumber += numText;
 
-    lastNumber += storedNum;
-    total += storedNum;
-    display.value += storedNum;
+      console.log("first num turn now");
+    } else if (!operator && lastNumTurn) {
+      lastNumber += numText;
+      console.log("lastnumturn now");
+    }
+
+    display.value += numText;
   });
 });
 
-//decimals
-decimalButton.addEventListener("click", () => {
-  const currentNumber = lastNumber;
-  if (!currentNumber.includes(".")) {
-    display.value += ".";
-  }
-});
-
-//operators
 operatorButtons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const op = btn.textContent.trim();
+  btn.addEventListener("click", (e) => {
+    let operators = ["+", "-", "*", "/"];
+    let op = e.target.textContent;
     const lastChar = display.value.slice(-1);
 
-    if (!operator) return;
+    if (operators.includes(lastChar)) {
+      return;
+    }
 
-    if ("+-*/".includes(lastChar)) return;
+    opArray.push(op);
+    console.log(opArray);
 
-    calculate();
+    if (operator && lastNumTurn) {
+      firstNumber = display.value; // Everything before operator
 
-    operator = op; // store the new operator
-    lastNumber = ""; // reset number being typed
-
+      console.log(firstNumber + " In operator function");
+    } else {
+      lastNumber = display.value.slice(firstNumber.length + 1);
+      calculate(firstNumber, lastNumber);
+      firstNumber = total;
+    }
+    operator = op;
     display.value += op;
   });
 });
-
-//equal operation
-equalButton.addEventListener("click", () => {
-  calculate();
-
-  display.value = total;
-  total = savedTotal.toString();
-
-  lastNumber = "";
-  operator = "";
-});
-
-// clear
-clearButton.addEventListener("click", () => {
-  lastNumber = "";
-  operator = "";
-  display.value = "";
-  total = 0;
-});
-
-//backspace
-deleteButton.addEventListener("click", () => {});
